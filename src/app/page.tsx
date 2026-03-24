53d43b1f430fc8a40dab0fba5e1270b13b41e939
Botan-linux <zeke000p@gmail.com>
2026-03-24 15:19:35 +0300

    Delete src/app directory

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export default function NotFound() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-4xl font-bold">404</h1>
-    </div>
-  )
-}
